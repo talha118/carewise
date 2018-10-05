c5193cad0b1a5432ce7af434fe05dc542b526587
talha118 <talha.yahya@yahoo.com>
2018-10-05 17:57:18 +0500

    heroku_deployment

--- carewise/settings.py
+++ carewise/settings.py
@@ -84,18 +84,18 @@ WSGI_APPLICATION = 'carewise.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 #heroku database
 
-# DATABASES = {
-#     'default': dj_database_url.config()
-# }
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # DATABASES['default'] =  dj_database_url.config()
 
