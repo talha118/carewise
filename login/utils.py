620188dffeed1e40cad0f470bd54bb994cf9f635
talha118 <talha.yahya@yahoo.com>
2018-10-06 00:25:34 +0500

    proxy added

--- login/utils.py
+++ login/utils.py
@@ -1,11 +1,17 @@
+import os
 import requests
 from carewise.settings import TABLEAU_IP
 
+proxyDict = {
+              "http"  : os.environ.get('FIXIE_URL', ''),
+              "https" : os.environ.get('FIXIE_URL', '')
+            }
 
 def trusted_authentication_tableau(requested_view, tableau_username):
-    response = requests.post(TABLEAU_IP, data = {'username': tableau_username})
+    response = requests.post(TABLEAU_IP, data = {'username': tableau_username}, proxies = proxyDict)
     ticket = ""
     final_url = ""
+
     if response.status_code == 200:
         if str(response.text) != "-1":
             ticket = str(response.text)
